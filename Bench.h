a83e4b34275edf9c6779aab08a9a6fe83ba5a269
TheWisp <feiteng@gmail.com>
2020-01-16 23:33:10 +0100

    Refine benchmarks

--- Bench.h
+++ Bench.h
@@ -51,70 +51,22 @@ auto getFactories()
 
 static auto fac = getFactories();
 
-void bench_same_class(bool shuffle_obj)
+void bench_classes(bool diff_class, bool shuffle_obj)
 {
 	std::random_device rd;
 	std::mt19937 rgen(rd());
 
-	std::cout << "Signal vs virtual " << (shuffle_obj ? "(SAME class, RANDOM object addr)" : "(SAME class, SEQUENTIAL object addr)") << "\n";
+	auto class_desc = diff_class ? "RANDOM classes" : "SAME class";
+	auto obj_desc = shuffle_obj ? "RANDOM objects" : "SEQUENTIAL objects";
+	std::cout << "Signal vs virtual " << "(" << class_desc << ", " << obj_desc << ")\n";
 
-	std::vector<Bar<0>> bars;
+	std::vector<std::unique_ptr<BarBase>> bars; //represents a typical setup for virtual-based observers: vector of interface
 	Foo foo;
-	bars.resize(100'000);
-	//connect in random order to emulate the random distribution of objects in memory.
-	std::vector<size_t> indices(100'000);
-	std::iota(indices.begin(), indices.end(), 0);
-	if (shuffle_obj) std::shuffle(indices.begin(), indices.end(), rgen);
+	if (diff_class)
+		for (int i = 0; i < 100'000; i++) bars.emplace_back(fac[rand() % 100]());
+	else
+		for (int i = 0; i < 100'000; i++) bars.emplace_back(fac[0]());
 
-	for (size_t i : indices)
-		bars[i].connect(&foo);
-
-	double signal_duration, call_duration, signal_lambda_duration;
-
-	{
-		auto start = std::chrono::high_resolution_clock::now();
-		foo.sig1(0.001f);
-		auto end = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> diff = end - start;
-		std::cout << "Signal (member func): " << std::chrono::duration_cast<std::chrono::microseconds>(diff).count() << " us\n";
-		signal_duration = diff.count();
-	}
-
-	{
-		auto start = std::chrono::high_resolution_clock::now();
-		foo.sig2(0.001f);
-		auto end = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> diff = end - start;
-		std::cout << "Signal (lambda): " <<std::chrono::duration_cast<std::chrono::microseconds>(diff).count() << " us\n";
-		signal_lambda_duration = diff.count();
-	}
-
-	{
-		auto start = std::chrono::high_resolution_clock::now();
-		for (size_t i : indices)
-			bars[i].vf(0.001f);
-		auto end = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> diff = end - start;
-		std::cout << "Virtual Call: " <<std::chrono::duration_cast<std::chrono::microseconds>(diff).count() << " us\n";
-		call_duration = diff.count();
-	}
-
-	std::cout << "Signal (member func) takes " << (int)(100 * signal_duration / call_duration) << "% time of virtual function calls \n";
-	std::cout << "Signal (lambda) takes " << (int)(100 * signal_lambda_duration / call_duration) << "% time of virtual function calls \n";
-	std::cout << "\n" << std::endl;
-}
-
-void bench_diff_classes(bool shuffle_obj)
-{
-	std::random_device rd;
-	std::mt19937 rgen(rd());
-
-	std::cout << "Signal vs virtual " << (shuffle_obj ? "(RANDOM classes, RANDOM object addr)" : "(RANDOM classes, SEQUENTIAL object addr)") << "\n";
-
-	std::vector<std::unique_ptr<BarBase>> bars;
-	Foo foo;
-	for (int i = 0; i < 100'000; i++)
-		bars.emplace_back(fac[rand() % 100]());
 	std::vector<size_t> indices(100'000);
 	std::iota(indices.begin(), indices.end(), 0);
 	if (shuffle_obj) std::shuffle(indices.begin(), indices.end(), rgen);
@@ -160,8 +112,8 @@ void bench_diff_classes(bool shuffle_obj)
 
 void bench()
 {
-	bench_same_class(false);
-	bench_same_class(true);
-	bench_diff_classes(false);
-	bench_diff_classes(true);
+	bench_classes(false, false);
+	bench_classes(false, true);
+	bench_classes(true, false);
+	bench_classes(true, true);
 }
