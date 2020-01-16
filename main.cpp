dd5b713984502c7ec05d203e53546b34a7760ddf
TheWisp <feiteng@gmail.com>
2020-01-16 19:58:04 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,205 @@
+#include <cassert>
+#include "signals.hpp"
+#include <functional>
+#include <iostream>
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <chrono>
+#include <memory>
+
+using namespace fteng;
+
+struct Foo
+{
+	signal<void(float)> sig;
+};
+
+struct Bar
+{
+	void onSig(float delta)
+	{
+		total += delta;
+	}
+
+	connection conn;
+
+	Bar(Foo& foo)
+	{
+		conn = foo.sig.connect<&Bar::onSig>(this);
+	}
+
+	volatile float total = 0.f;
+};
+
+namespace ReentranceTest
+{
+	struct Foo
+	{
+		signal<void(int count)> sig;
+	};
+
+	struct Bar
+	{
+		void onSig(int count);
+
+		connection conn;
+		Foo* m_foo;
+		int x = 0;
+		bool flag;
+
+		Bar(Foo& foo, bool flag = true)
+			: flag(flag)
+		{
+			conn = foo.sig.connect<&Bar::onSig>(this);
+			m_foo = &foo;
+		}
+	};
+
+	std::vector<std::unique_ptr<Bar>> dyn_connections;
+
+	void test()
+	{
+		dyn_connections.clear();
+
+		Foo foo;
+		for (int i = 0, n = rand() % 10; i < n; i++)
+			dyn_connections.emplace_back(new Bar(foo, false));
+
+		Bar bar(foo);
+
+		foo.sig(3);
+		assert(bar.x == 3);
+	}
+
+	inline void Bar::onSig(int count)
+	{
+		std::cout << "Calling " << this << std::endl;
+		if (flag) {
+			if (count > 0) {
+				this->x++;
+
+				//purposely add or remove listeners
+				if (rand() & 1)
+					dyn_connections.emplace_back(new Bar(*m_foo, false));
+				else if (!dyn_connections.empty())
+					dyn_connections.erase(dyn_connections.begin() + rand() % dyn_connections.size());
+				m_foo->sig(count - 1);
+			}
+		}
+	}
+}
+
+namespace FreeFuncTest
+{
+	signal<void(int count)> sig;
+	int c = 0;
+	void onSig(int count) 
+	{
+		c += count;
+	}
+
+	connection conn;
+	void test()
+	{
+		conn = sig.connect<onSig>();
+		sig(33);
+		assert(c == 33);
+	}
+}
+
+namespace LambdaTest
+{
+	signal<void(int count)> sig;
+	connection conn;
+	int glob_c = 0;
+
+	void test()
+	{
+		int c = 0;
+		//small capture lambda test
+		conn = sig.connect([&](int count) { c += count; });
+		sig(33);
+		assert(c == 33);
+		
+		//non-captured lambda test
+		conn = sig.connect([](int count) { 
+			glob_c += count; 
+		});
+		sig(32);
+		assert(glob_c == 32);
+
+		int x = 100;
+		int y = 100;
+		//large lambda test
+		auto large_lambda = [&](int count) {
+			x += y + count;
+		};
+		static_assert(std::is_trivially_destructible_v<decltype(large_lambda)>);
+		static_assert(sizeof large_lambda > 8);
+		conn = sig.connect(large_lambda);
+		sig(32);
+		assert(x == 232);
+
+		auto large_lambda_2 = [&, v = std::vector{1,2,3}](int count) {
+			x = *std::max_element(v.begin(), v.end()) + count;
+		};
+		static_assert(!std::is_trivially_destructible_v<decltype(large_lambda_2)>);
+		conn = sig.connect(large_lambda_2);
+		sig(32);
+		assert(x == 35);
+	}
+}
+
+void test()
+{
+	//basic usage, class
+	{
+		Foo foo;
+		Bar bar(foo);
+		foo.sig(3.14f);
+		assert(bar.total == 3.14f);
+		foo.sig(3.14f);
+		assert(bar.total == 6.28f);
+	}
+
+	//scope safety (connection)
+	{
+		Foo foo;
+		{
+			Bar bar1(foo), * bar3;
+			{
+				Bar bar2(foo);
+				bar3 = new Bar(foo);
+				foo.sig(1.f);
+				assert(bar1.total == 1);
+				assert(bar2.total == 1);
+				assert(bar3->total == 1);
+			}
+			foo.sig(1.f);
+			assert(bar1.total == 2);
+			assert(bar3->total == 2);
+		}
+		foo.sig(1.f);
+	}
+
+	//reentrance and modification safety
+	ReentranceTest::test();
+
+	//basic usage, free function
+	FreeFuncTest::test();
+
+	//basic usage, lambda
+	LambdaTest::test();
+}
+
+namespace BenchEventVector
+{
+#include "BenchEventVsCall.h"
+}
+
+int main()
+{
+	test();
+	BenchEventVector::test();
+}
---
+++ signals.hpp
@@ -0,0 +1,222 @@
+#pragma once
+#include <cstdlib>
+#include <vector>
+
+namespace fteng
+{
+	struct conn_base;
+	struct sig_base
+	{
+		struct call
+		{
+			void* object;
+			void* func;
+		};
+
+		// space can be optimized by using "struct of array" containers
+		std::vector<call>       calls;
+		std::vector<conn_base*> conns;
+
+		// space can be optimized by stealing 2 unused bits from the vector size
+		bool calling = false;
+		bool dirty = false;
+
+		~sig_base();
+	};
+
+	template<typename F> struct signal;
+
+	struct conn_base
+	{
+		sig_base* sig;
+		size_t    idx;
+
+		conn_base(sig_base* sig, size_t idx) : sig(sig), idx(idx) {}
+
+		virtual ~conn_base()
+		{
+			if (sig)
+			{
+				sig->calls[idx].object = nullptr;
+				sig->calls[idx].func = nullptr;
+				sig->conns[idx] = nullptr;
+				sig->dirty = 1;
+			}
+		}
+	};
+
+	template<typename T>
+	struct conn_nontrivial : conn_base
+	{
+		using conn_base::conn_base;
+
+		virtual ~conn_nontrivial()
+		{
+			if (sig)
+				reinterpret_cast<T*>(&sig->calls[idx].object)->~T();
+		}
+	};
+
+	sig_base::~sig_base()
+	{
+		for (conn_base* c : conns)
+			if (c) c->sig = nullptr;
+	}
+
+	struct [[nodiscard]] connection
+	{
+		conn_base * ptr = nullptr;
+
+		void disconnect()
+		{
+			if (ptr)
+			{
+				delete ptr;
+				ptr = nullptr;
+			}
+		}
+
+		connection(conn_base* ptr) : ptr(ptr) {}
+
+		connection() = default;
+
+		~connection()
+		{
+			disconnect();
+		}
+		connection(const connection&) = delete;
+
+		connection& operator=(const connection&) = delete;
+
+		connection(connection&& other) noexcept
+		{
+			*this = std::move(other);
+		}
+
+		connection& operator=(connection&& other) noexcept
+		{
+			disconnect();
+			ptr = other.ptr;
+			other.ptr = nullptr;
+			return *this;
+		}
+	};
+
+
+	template<typename ... A>
+	struct signal<void(A...)> : sig_base
+	{
+		template<typename ... ActualArgsT>
+		void operator()(ActualArgsT&& ... args)
+		{
+			bool recursion = calling;
+			if (!calling) calling = 1;
+			for (size_t i = 0; i < calls.size(); ++i)
+			{
+				auto& cb = calls[i];
+				if (cb.func)
+				{
+					if (cb.object)
+						reinterpret_cast<void(*)(void*, A...)>(cb.func)(&cb.object, std::forward<ActualArgsT>(args)...);
+					else
+						reinterpret_cast<void(*)(A...)>(cb.func)(std::forward<ActualArgsT>(args)...);
+				}
+			}
+
+			if (!recursion)
+			{
+				calling = 0;
+
+				if (dirty)
+				{
+					dirty = 0;
+					//remove all empty slots while patching the stored index in the connection
+					size_t sz = 0;
+					for (size_t i = 0, n = conns.size(); i < n; ++i)
+					{
+						if (conns[i]) {
+							conns[sz] = conns[i];
+							calls[sz] = calls[i];
+							conns[sz]->idx = sz;
+							++sz;
+						}
+					}
+					conns.resize(sz);
+					calls.resize(sz);
+				}
+			}
+		}
+
+		template<auto func, class C>
+		[[nodiscard]] connection connect(C* object)
+		{
+			size_t idx = conns.size();
+			auto& call = calls.emplace_back();
+			call.object = object;
+			call.func = +[](void* obj, A ... args) {((*static_cast<C * *>(obj))->*func)(args...); };
+			conn_base* conn = new conn_base(this, idx);
+			conns.emplace_back(conn);
+			return { conn };
+		}
+
+		template<auto func>
+		[[nodiscard]] connection connect()
+		{
+			return connect(func);
+		}
+
+		[[nodiscard]] connection connect(void(*func)(A...))
+		{
+			size_t idx = conns.size();
+			auto& call = calls.emplace_back();
+			call.func = func;
+			conn_base* conn = new conn_base(this, idx);
+			conns.emplace_back(conn);
+			return { conn };
+		}
+
+		template<typename F>
+		[[nodiscard]] connection connect(F functor)
+		{
+			if constexpr (std::is_convertible_v<F, void(*)(A...)>)
+			{
+				return connect(+functor);
+			}
+			else if constexpr (sizeof(F) <= sizeof(void*))
+			{
+				//copy the functor. 
+				size_t idx = conns.size();
+				auto& call = calls.emplace_back();
+				call.func = +[](void* obj, A ... args) { reinterpret_cast<F*>(obj)->operator()(args...); };
+				new (&call.object) F(functor);
+				using conn_t = std::conditional_t<std::is_trivially_destructible_v<F>, conn_base, conn_nontrivial<F>>;
+				conn_base* conn = new conn_t(this, idx);
+				conns.emplace_back(conn);
+				return { conn };
+			}
+			else
+			{
+				struct unique
+				{
+					F* ptr;
+
+					~unique()
+					{
+						delete ptr;
+					}
+
+					unique(unique&) = delete;
+				};
+
+				size_t idx = conns.size();
+				auto& call = calls.emplace_back();
+				call.func = (void*)+[](void* obj, A ... args) { reinterpret_cast<unique*>(obj)->ptr->operator()(args...); };
+				new (&call.object) unique{ new F(functor) };
+				conn_base* conn = new conn_nontrivial<unique>(this, idx);
+				conns.emplace_back(conn);
+				return { conn };
+			}
+		}
+	};
+}
+
